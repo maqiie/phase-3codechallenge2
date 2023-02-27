7dc354da6771191bebeb7438bd458672f393f0cc
maqiie <99504010+maqiie@users.noreply.github.com>
2023-02-27 23:57:57 +0300

    worked on reveiws

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :product
+
+    def print_review
+    #     puts "Review for #{product.name} by #{user.name}: #{self.star_rating}. #{self.comment}
+    # end
+    end
+end
